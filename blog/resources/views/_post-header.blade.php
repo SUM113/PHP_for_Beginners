9198287b338b2f930ad13f478c81f6d7cdb8ec55
sum113 <sajadmulk000@gmail.com>
2024-01-23 14:04:37 +0500

    commented version

--- blog/resources/views/_post-header.blade.php
+++ blog/resources/views/_post-header.blade.php
@@ -8,37 +8,14 @@
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!--  Category -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-
-            <x-dropdown>
-
-                <x-slot name="trigger">
-                    <button @click= "show = !show"  class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left inline-flex">
-                        {{isset($category_selected) ? ucwords($category_selected->name) : 'Categories'}}
-
-                        <x-icon name="downArrow" class="absolute pointer-events-none text-bold"/>
-
-                    </button>
-                </x-slot>
-
-                <x-dropdownItem href="/" :actve=" request()->routeIs('home')">All</x-dropdownItem>
-                @foreach($categories as $category)
-                    <x-dropdownItem href="/category/{{$category->slug}}"
-                                    :active="isset($category_selected) && $category_selected->is($category)"
-                    >{{$category->name. "\n"}}
-                    </x-dropdownItem>
-
-
-{{--                    <x-dropdownItem--}}
-{{--                      href="/category/{{$category->slug}}"--}}
-{{--                      :active='{{isset($category_selected) && $category->id === $category_selected->id}}'--}}
-{{--                    >{{ucwords($category->name)}}--}}
-{{--                  </x-dropdownItem>--}}
-
-                @endforeach
-
-            </x-dropdown>
-
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+            <form method="GET" action="#">
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="Find something"
+                    class="bg-transparent placeholder-black font-semibold text-sm">
+            </form>
         </div>
 
         <!-- Other Filters -->
@@ -64,14 +41,40 @@
 {{--        </div>--}}
 
         <!-- Search -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
-            <form method="GET" action="#">
-                <input
-                    type="text"
-                    name="search"
-                    placeholder="Find something"
-                    class="bg-transparent placeholder-black font-semibold text-sm">
-            </form>
+        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
+
+       {{-- will create the blade component to extract the dropdown into its own component.--}}
+
+
+            <x-dropdown>
+
+                <x-slot name="trigger">
+                    <button @click= "show = !show"  class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left inline-flex">
+                        {{isset($category_selected) ? ucwords($category_selected->name) : 'Categories'}}
+
+                        <x-icon name="downArrow" class="absolute pointer-events-none text-bold"/>
+
+                    </button>
+                </x-slot>
+
+                <x-dropdownItem href="/" :actve=" request()->routeIs('home')">All</x-dropdownItem>
+                @foreach($categories as $category)
+                    <x-dropdownItem href="/category/{{$category->slug}}"
+                                    :active="isset($category_selected) && $category_selected->is($category)"
+                    >{{$category->name. "\n"}}
+                    </x-dropdownItem>
+
+
+                    {{--                    <x-dropdownItem--}}
+                    {{--                      href="/category/{{$category->slug}}"--}}
+                    {{--                      :active='{{isset($category_selected) && $category->id === $category_selected->id}}'--}}
+                    {{--                    >{{ucwords($category->name)}}--}}
+                    {{--                  </x-dropdownItem>--}}
+
+                @endforeach
+
+            </x-dropdown>
+
         </div>
     </div>
 </header>
