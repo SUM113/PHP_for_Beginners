b801311a635ba469f47c73bc5caef488095bf95d
sum113 <sajadmulk000@gmail.com>
2024-01-26 15:09:19 +0500

    Ist commit:Designed the post layout

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use function Termwind\renderUsing;
+
+class SessionController extends Controller
+{
+    public function destroy()
+    {
+        auth()->logout();
+        return redirect(request()->routeIs('home'))->with('success','Goodbye 👋👋👋');
+
+    }
+
+    public function create(){
+        return view('session.create');
+
+    }
+    public function session(){
+
+        session()->regenerate();
+        $userdata=request()->validate([
+            'email'=> 'required|email',
+            'password'=> 'required',
+        ]);
+
+        if(!auth()->attempt($userdata))
+            return back()->withErrors(['email'=>'Credential could not be verified.'])->withInput();
+        else
+            return redirect(request()->routeIs('home'))->with('success','Welcome Back');
+    }
+}
